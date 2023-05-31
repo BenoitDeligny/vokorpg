36abe7e2c68553eb481f7977f43207bf3c557fc1
Benoit Deligny <benoit.deligny@manomano.com>
2023-05-31 18:22:02 +0200

    add action usecas

---
+++ src/main/java/vokorpgback/feature/action/application/ActionUseCase.kt
@@ -0,0 +1,13 @@
+package vokorpgback.feature.action.application
+
+class ActionUseCase {
+    fun handle(): Boolean {
+        // TODO
+        // add parameters -> type (climbing, swimming, ...), difficulty, ability involved, bonus (knowledge, items, ...)
+        // roll 2 dice
+        // add bonus
+        // compare to difficulty
+        // return true or false
+        return false;
+    }
+}
