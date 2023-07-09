b7db1920ba4997a59baa5bcd682367978a30943d
Benoit Deligny <benoit.deligny@manomano.com>
2023-07-09 14:22:41 +0200

    entity

--- src/main/java/vokorpgback/feature/character/infra/entity/CharacterEntity.java
+++ src/main/java/vokorpgback/feature/character/infra/entity/CharacterEntity.java
@@ -1,4 +1,51 @@
 package vokorpgback.feature.character.infra.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "legendary_character")
 public class CharacterEntity {
+    // TODO find a better primary key
+    @EmbeddedId
+    private CharacterIdentity characterIdentity;
+
+    public CharacterIdentity getCharacterIdentity() {
+        return characterIdentity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CharacterEntity that)) return false;
+        return Objects.equals(characterIdentity, that.characterIdentity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(characterIdentity);
+    }
+
+    @Embeddable
+    public static class CharacterIdentity implements Serializable {
+
+        @NotNull
+        @Column(name = "name", nullable = false)
+        private String name;
+
+        @NotNull
+        @Column(name = "age", nullable = false)
+        private String age;
+
+        public CharacterIdentity() {
+        }
+
+        public CharacterIdentity(@NotNull String name, @NotNull String age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
 }
